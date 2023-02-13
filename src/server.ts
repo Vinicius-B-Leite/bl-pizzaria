a2dd76d0e338caedbbe41653748c17b6f1ce416c
Vinicius B. Leite1 <viniciusbleite21@gmail.com>
2023-02-13 09:16:12 -0300

    feature: tratando a resposta de erro da api

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,28 @@
-import express, { Response, Request } from 'express'
+import express, { Response, Request, NextFunction } from 'express'
 import router from './routes'
-
-
+import 'express-async-errors'
+import cors from 'cors'
 const app = express()
+
+
+
+
 app.use(express.json())
+app.use(cors())
 app.use(router)
+
+
+app.use((err: Error, req: Request, res: Response, nxt: NextFunction) => {
+    if (err instanceof Error) {
+        //instanceof == typeof
+        return res.status(400).json({ error: err.message })
+    }
+
+    return res.status(500).json({
+        status: 'error',
+        message: 'Internal server errror'
+    })
+})
+
 
 app.listen(3333, () => console.log('Server is running'))
