b655c2b4921285a13e7f3b6923b6e7221db7dd4d
Vinicius B. Leite1 <viniciusbleite21@gmail.com>
2023-02-16 16:00:40 -0300

    structure: build

--- build/config/multer.d.ts
+++
@@ -1,7 +0,0 @@
-import multer from "multer";
-declare const _default: {
-    upload(folder: string): {
-        storage: multer.StorageEngine;
-    };
-};
-export default _default;
--- build/middlewares/isAutenticated.d.ts
+++
@@ -1,2 +0,0 @@
-import { NextFunction, Request, Response } from "express";
-export declare function isAuthenticated(req: Request, res: Response, next: NextFunction): void | Response<any, Record<string, any>>;
